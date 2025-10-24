b9810b4f2dd8cc823605705dfeb5ae91e47b91fe
Dustin Tran <dustintr@udel.edu>
2025-10-24 13:00:30 -0400

    typo

--- apps/web-start/src/components/Card/Card.tsx
+++ apps/web-start/src/components/Card/Card.tsx
@@ -132,7 +132,7 @@ export default function Card({ assignment, onClick }: CardProps) {
       </div>
 
       <style>{`
-C        .transform-style-preserve-3d {
+        .transform-style-preserve-3d {
           transform-style: preserve-3d;
         }
         .backface-hidden {
